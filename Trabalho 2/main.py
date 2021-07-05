b9da829a437c641bb471191cd30431e6c7930ba9
mpinho <mpinho@alunos.utfpr.edu.br>
2021-07-05 11:28:15 -0300

    algoritmos funcionando separadamente; Comparador implementado; saidas diferentes

--- Trabalho 2/main.py
+++ Trabalho 2/main.py
@@ -16,21 +16,20 @@ import cv2
 INPUT_IMAGE =  'a01.bmp'  #documento-3mp
 JANELA_H = 5
 JANELA_W = 5
-
+MODE = 0 # 0:INGENUO, 1:SEPRAVEL, 2:INTEGRAL, 3:TODOS
 #===============================================================================
 
-def comparador(img_1,img_2):
+def comparador(img_1,img_2,rows, cols):
     
-    rows, cols, channels = img_1.shape
-    img_ = img_1
-
-    print (img_2)
+    sum = 0
 
     for linha in range(rows):
         for coluna in range(cols):
-            img_[linha][coluna] = img_1[linha][coluna] - img_2[linha][coluna] 
+            if(img_1[linha][coluna] == img_2[linha][coluna] ):
+                sum += 1
+    sum = sum/(rows*cols)
+    print("As imagens sao ", sum*100, "% parecidas")
 
-    return img_
 
 def opencvblur(img):
     # blur (img, largura, altura)
@@ -43,7 +42,6 @@ def ingenuo(img):
     img_out = img
 
     for linha in range(int(JANELA_H/2),rows-int(JANELA_H/2)):        
-        print("Linhas:",linha)
         for coluna in range(int(JANELA_W/2),cols-int(JANELA_W/2)):
             soma = 0
             for y in range(linha-int(JANELA_H/2),linha+int(JANELA_H/2)+1):
@@ -62,7 +60,6 @@ def separavel(img):
     
     # HORIZONTAL
     for linha in range(int(JANELA_H/2),rows-int(JANELA_H/2)):        
-        print("Linhas:",linha)
         for coluna in range(int(JANELA_W/2),cols-int(JANELA_W/2)):
             soma = 0
             # iteracao na horizontal
@@ -72,7 +69,6 @@ def separavel(img):
 
     # VERTICAL
     for linha in range(int(JANELA_H/2),rows-int(JANELA_H/2)):        
-        print("Linhas:",linha)
         for coluna in range(int(JANELA_W/2),cols-int(JANELA_W/2)):
             soma = 0
             # iteracao na vertical
@@ -82,93 +78,73 @@ def separavel(img):
 
     return img_out
 
+def createBuffer(rows, cols, img_aux):
+    buffer = img_aux
+    
+    ## Percorro a coluna 
+    ## Primeira coluna da imagem ja esta formada
+    for linha in range(rows):        
+        for coluna in range(1,cols):
+            buffer[linha][coluna] = img_aux[linha][coluna] + buffer[linha][coluna-1]
+
+    ## Percorro por linha 
+    for linha in range(1,rows):        
+        for coluna in range(cols):
+            buffer[linha][coluna] = buffer[linha][coluna] + buffer[linha-1][coluna]
+    
+    return buffer
+
+
 def calculaMedia(linha, coluna, buffer_, rows, cols):
+    media = 0
+
     ponto_superior = linha - int(JANELA_H/2) - 1
+    if(ponto_superior < 0 ):
+        ponto_superior = -1
             
     ponto_inferior = linha + int(JANELA_H/2)
     if (ponto_inferior > rows - 1 ):
         ponto_inferior = rows - 1
 
     ponto_esquerda = coluna - int(JANELA_W/2) - 1 
+    if(ponto_esquerda < 0 ):
+        ponto_esquerda = -1
     
     ponto_direita = coluna + int(JANELA_W/2) 
     if (ponto_direita > cols - 1 ):
         ponto_direita = cols - 1
 
-    janela_w = ponto_direita + 1
-    if(ponto_esquerda >= 0 ):
-        janela_w = ponto_direita - ponto_esquerda
-
-    janela_h = ponto_inferior + 1
-    if(ponto_superior >= 0 ):
-        janela_h = ponto_inferior - ponto_superior
-
+    janela_w = ponto_direita - ponto_esquerda
+    janela_h = ponto_inferior - ponto_superior
+    
     # Definindo os 4 pontos
-    print("-------")
-    print("coluna: ", coluna)
-    print("ponto_superior: ", ponto_superior)
-    print("ponto_inferior: ", ponto_inferior)
-    print("ponto_esquerda: ", ponto_esquerda)
-    print("ponto_direita: ", ponto_direita)
-    print("janela_w: ", janela_w)
-    print("janela_h: ", janela_h)
-
-    print("soma: ", buffer_[ponto_inferior][ponto_direita])
     soma = buffer_[ponto_inferior][ponto_direita]
     if(ponto_superior >= 0 ):
         soma = soma - buffer_[ponto_superior][ponto_direita]
-        print("sup: diminui ", buffer_[ponto_superior][ponto_direita], " -> soma: ", soma)
     if(ponto_esquerda >= 0 ):
         soma = soma - buffer_[ponto_inferior][ponto_esquerda]
-        print("esq: diminui ", buffer_[ponto_inferior][ponto_esquerda], " -> soma: ", soma)
     if(ponto_superior >= 0 and ponto_esquerda >= 0 ):
         soma = soma + buffer_[ponto_superior][ponto_esquerda]
-        print("dois: soma ", buffer_[ponto_superior][ponto_esquerda], " -> soma: ", soma)
     
     media = soma / (janela_w * janela_h)
-    if(media != 1):
-        print("\tsoma: ", soma)
-        print("\tmedia: ", media)
-    
     return media
 
 def integral(img):
 
-    img_aux = np.where( img > 0, 1, 1)
     rows, cols, channels = img.shape
-    img_out = img_aux
-    buffer = img_aux
     
     # Imagem integral
-    ## Percorro a coluna 
-    ## Primeira coluna da imagem ja esta formada
+    buffer = createBuffer(rows, cols, img)
     
-    for linha in range(rows):        
-        print("Linhas:",linha)
-        for coluna in range(1,cols):
-            buffer[linha][coluna] = img_aux[linha][coluna] + buffer[linha][coluna-1]
+    #img_out = buffer
+    img_out = np.zeros((rows, cols))
 
-    ## Percorro por linha 
-    for linha in range(1,rows):        
-        print("Linhas:",linha)
-        for coluna in range(cols):
-            buffer[linha][coluna] = buffer[linha][coluna] + buffer[linha-1][coluna]
-    
-    # for linha in range(rows):        
-    #     for coluna in range(cols):    
-    #         if(buffer[linha][coluna] != (linha+1)*(coluna+1)):
-    #             print("buffer[", linha, "][", coluna, "]: ", buffer[linha][coluna])
-    
     # Janela deslizante
     for linha in range(rows):        
-        print("Linhas:",linha)
-        # for y in range(rows):        
-        #     for x in range(cols):    
-        #         if(buffer[y][x] != (y+1)*(x+1)):
-        #             print("buffer[", y, "][", x, "]: ", buffer[y][x])
         for coluna in range(cols):           
-            img_out[linha][coluna] = calculaMedia(linha, coluna, buffer, rows, cols)
-
+            a = calculaMedia(linha, coluna, buffer, rows, cols)
+            img_out[linha][coluna] = a
+            
     return img_out
 
 def main ():
@@ -190,25 +166,51 @@ def main ():
 
     img_opencv = opencvblur(img)
     
-
     start_time = timeit.default_timer ()
    
-    # Colocar funcoes aqui dentro
-    # print("Iniciou ingenuo")
-    # img_out_ingenuo = ingenuo(img)
+    rows_, cols_, channels = img.shape
+    if(MODE == 0  or MODE == 3):
+        # INGENUO
+        print("Iniciou ingenuo")
+        img_out_ingenuo = ingenuo(img)
+        print ('Tempo: %f' % (timeit.default_timer () - start_time))
+        print("comparador (openCV e ingenuo: ")
+        comparador(img_out_ingenuo,img_opencv,rows_, cols_)
 
-    # print("Iniciou separavel")
-    # img_out_separavel = separavel(img)
-
-    img_out_integral = integral(img)
-    print ('Tempo: %f' % (timeit.default_timer () - start_time))
-
+    if(MODE == 1 or MODE == 3):
+        # SEPARAVEL
+        #start_time = timeit.default_timer ()
+        print("Iniciou separavel")
+        img_out_separavel = separavel(img)
+        print ('Tempo: %f' % (timeit.default_timer () - start_time))
+        print("comparador (openCV e sepravel: ")
+        comparador(img_out_separavel,img_opencv,rows_, cols_)
+        
+    if(MODE == 2 or MODE == 3):
+        # INTEGRAL
+        #start_time = timeit.default_timer ()
+        print("Iniciou Integral")
+        img_out_integral = integral(img)
+        print ('Tempo: %f' % (timeit.default_timer () - start_time))
+        print("comparador (openCV e integral: ")
+        comparador(img_out_integral,img_opencv,rows_, cols_)
+        
     #img_comparada = comparador(img_out_separavel,img_opencv)
-    cv2.imwrite ('out.png', img_out_integral*255)
-    cv2.imshow ('saida borrada ', img_out_integral)
+
+    cv2.imwrite ('out_opencv.png', img_opencv*255)
     cv2.imshow ('saida opencv',img_opencv)
-    # cv2.imshow ('comparador',img_comparada)
-    cv2.imwrite ('out.png', img_out_integral*255)
+
+    if(MODE == 0  or MODE == 3):
+        cv2.imwrite ('out_ingenuo.png', img_out_ingenuo*255)
+        cv2.imshow ('saida ingenuo',img_out_ingenuo)
+    if(MODE == 1 or MODE == 3):
+        cv2.imwrite ('out_separavel.png', img_out_separavel*255)
+        cv2.imshow ('saida separavel', img_out_separavel)
+    if(MODE == 2 or MODE == 3):
+        cv2.imwrite ('out_integral.png', img_out_integral*255)
+        cv2.imshow ('saida integral',img_out_integral)
+
+
     cv2.waitKey ()
     cv2.destroyAllWindows ()
 
