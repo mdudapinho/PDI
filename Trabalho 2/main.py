b73b3d8216086570fe5b7b2e9b4b4ce0ae3965a3
mglucas <luc0797@gmail.com>
2021-07-05 15:55:44 -0300

    Ajuste em variaveis, chamada de funcao e comentarios

--- Trabalho 2/main.py
+++ Trabalho 2/main.py
@@ -13,29 +13,47 @@ import cv2
 
 #===============================================================================
 
-INPUT_IMAGE =  'a01.bmp'  #documento-3mp
-JANELA_H = 5
-JANELA_W = 5
-MODE = 0 # 0:INGENUO, 1:SEPRAVEL, 2:INTEGRAL, 3:TODOS
-LIMITE = 0.004# numero magico
+INPUT_IMAGE =  r'.\\Exemplos\\b01 - Original.bmp'  #documento-3mp
+JANELA_H = 3
+JANELA_W = 3
+MODE = 2 # 0:INGENUO, 1:SEPRAVEL, 2:INTEGRAL, 3:TODOS
+LIMITE = 0.004 # Indice que limita a diferenca por pixel no comparador
  
 #===============================================================================
 
-def comparador(img_1,img_2,rows, cols):
+def comparador(img_1,img_2,rows, cols, integral = False):
     sum = 0
 
-    #desconta as margens
-    r = rows-int(JANELA_H/2) - int(JANELA_H/2)
-    c = cols-int(JANELA_W/2) - int(JANELA_W/2)
-    
-    for linha in range(int(JANELA_H/2),rows-int(JANELA_H/2)):        
-        for coluna in range(int(JANELA_W/2),cols-int(JANELA_W/2)):
-            dif = img_1[linha][coluna] - img_2[linha][coluna] 
-            if(dif < LIMITE):
-                sum += 1
+    #Se nao for do algoritmo imagens integrais, descarta as bordas
+    if(not integral):
+        #desconta as margens
+        r = rows-int(JANELA_H/2)*2
+        c = cols-int(JANELA_W/2)*2
+        
+        # Compara pixel a pixel
+        for linha in range(int(JANELA_H/2),rows-int(JANELA_H/2)):        
+            for coluna in range(int(JANELA_W/2),cols-int(JANELA_W/2)):
+                dif = img_1[linha][coluna] - img_2[linha][coluna] 
+                if(dif < LIMITE):
+                    sum += 1
 
-    sum = sum/(r*c)
-    print("\t\tAs imagens sao ", sum*100, "% parecidas")
+        sum = sum/(r*c)
+        print("\t\tAs imagens sao ", round(sum*100, 2), "% parecidas")
+        return
+
+    # Se imagensIntegrais, compara a imagem inteira sem excluir o 
+    # tamanho das janelas das bordas
+    else:
+        # Compara pixel a pixel
+        for linha in range(rows):        
+            for coluna in range(cols):
+                dif = img_1[linha][coluna] - img_2[linha][coluna]
+                if(dif < LIMITE):
+                    sum += 1
+
+        sum = sum / (rows * cols)
+        print("\t\tAs imagens sao ", round(sum*100, 2), "% parecidas")
+        return
 
 def opencvblur(img):
     # blur (img, largura, altura)
@@ -87,52 +105,58 @@ def separavel(img):
 def createBuffer(rows, cols, img_aux):
     buffer = img_aux
     
-    ## Percorro a coluna 
+    ## Percorre na linha, acumulando os valores a esquerda 
     ## Primeira coluna da imagem ja esta formada
     for linha in range(rows):        
         for coluna in range(1,cols):
+            # Buffer = Intensidade no pixel + Pixel a esquerda
             buffer[linha][coluna] = img_aux[linha][coluna] + buffer[linha][coluna-1]
 
-    ## Percorro por linha 
+    ## Percorro por linha
+    ## Acumulando o valor que esta em cima do pixel no buffer
+    ## Completa a imagem integral 
     for linha in range(1,rows):        
         for coluna in range(cols):
+            # Buffer = Intensidade no buffer + Valor do buffer em cima
             buffer[linha][coluna] = buffer[linha][coluna] + buffer[linha-1][coluna]
     
     return buffer
 
-
 def calculaMedia(linha, coluna, buffer_, rows, cols):
     media = 0
 
-    ponto_superior = linha - int(JANELA_H/2) - 1
-    if(ponto_superior < 0 ):
-        ponto_superior = -1
+    superior = linha - int(JANELA_H/2) - 1
+    if(superior < 0 ):
+        superior = -1
             
-    ponto_inferior = linha + int(JANELA_H/2)
-    if (ponto_inferior > rows - 1 ):
-        ponto_inferior = rows - 1
+    inferior = linha + int(JANELA_H/2)
+    if (inferior > rows - 1 ):
+        inferior = rows - 1
 
-    ponto_esquerda = coluna - int(JANELA_W/2) - 1 
-    if(ponto_esquerda < 0 ):
-        ponto_esquerda = -1
+    esquerda = coluna - int(JANELA_W/2) - 1 
+    if(esquerda < 0 ):
+        esquerda = -1
     
-    ponto_direita = coluna + int(JANELA_W/2) 
-    if (ponto_direita > cols - 1 ):
-        ponto_direita = cols - 1
+    direita = coluna + int(JANELA_W/2) 
+    if (direita > cols - 1 ):
+        direita = cols - 1
 
-    janela_w = ponto_direita - ponto_esquerda
-    janela_h = ponto_inferior - ponto_superior
-    
+    # Estabelece os limites da janela 
+    # Quando nas bordas, a janela e menor
+    janela_w = direita - esquerda
+    janela_h = inferior - superior
+
     # Definindo os 4 pontos
-    soma = buffer_[ponto_inferior][ponto_direita]
-    if(ponto_superior >= 0 ):
-        soma = soma - buffer_[ponto_superior][ponto_direita]
-    if(ponto_esquerda >= 0 ):
-        soma = soma - buffer_[ponto_inferior][ponto_esquerda]
-    if(ponto_superior >= 0 and ponto_esquerda >= 0 ):
-        soma = soma + buffer_[ponto_superior][ponto_esquerda]
+    soma = buffer_[inferior][direita]
+    if(superior >= 0 ):
+        soma = soma - buffer_[superior][direita]
+    if(esquerda >= 0 ):
+        soma = soma - buffer_[inferior][esquerda]
+    if(superior >= 0 and esquerda >= 0 ):
+        soma = soma + buffer_[superior][esquerda]
     
     media = soma / (janela_w * janela_h)
+    
     return media
 
 def integral(img):
@@ -142,15 +166,14 @@ def integral(img):
     # Imagem integral
     buffer = createBuffer(rows, cols, img)
     
-    #img_out = buffer
+    #img_out com zeros
     img_out = np.zeros((rows, cols))
 
     # Janela deslizante
     for linha in range(rows):        
         for coluna in range(cols):           
-            a = calculaMedia(linha, coluna, buffer, rows, cols)
-            img_out[linha][coluna] = a
-            
+            img_out[linha][coluna] = calculaMedia(linha, coluna, buffer, rows, cols)
+
     return img_out
 
 def main ():
@@ -164,11 +187,10 @@ def main ():
     img = img.reshape ((img.shape [0], img.shape [1], 1))
     img = img.astype (np.float32) / 255
 
-    # Mantém uma cópia colorida para desenhar a saída.
+    # Mantem uma copia colorida para desenhar a saida
     img_color = cv2.cvtColor (img, cv2.COLOR_GRAY2BGR)
 
-    cv2.imshow ('img entrada opencv',img)
-    cv2.waitKey ()
+    # Calcula filtro com OpenCV
     img_out_opencv = opencvblur(img)
     
     rows_, cols_, channels = img.shape
@@ -210,11 +232,12 @@ def main ():
         print("Iniciou Integral")
         img_out_integral = integral(img_integral)
         print ('\tTempo: %f' % (timeit.default_timer () - start_time))
-        print("\tcomparador (openCV e integral: ")
-        comparador(img_out_integral,img_out_opencv,rows_, cols_)
+        print("\tcomparador (openCV e integral): ")
+        comparador(img_out_integral,img_out_opencv,rows_, cols_,True)
         cv2.imwrite ('out_integral.png', img_out_integral*255)
         cv2.imshow ('saida integral',img_out_integral)
 
+    # Comparacao com filtro do openCV
     cv2.imwrite ('out_opencv.png', img_out_opencv*255)
     cv2.imshow ('saida opencv',img_out_opencv)
 
