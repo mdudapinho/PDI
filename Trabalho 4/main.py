b216786321e80f6fb5f58d7c41fdd74918c7b985
mpinho <mpinho@alunos.utfpr.edu.br>
2021-07-28 11:10:34 -0300

    uso do filtro gaussinao. Sigma calculado. Diminuição do tempo de Erosão e Dilatação

--- Trabalho 4/main.py
+++ Trabalho 4/main.py
@@ -17,92 +17,11 @@ from math import sqrt
 INPUT_IMAGE =  [r"./60.bmp", r"./82.bmp", r"./114.bmp", r"./150.bmp", r"./205.bmp"]
 #INPUT_IMAGE =  [r"./60.bmp"]
 
-SIGMA = 100
+JANELA_EROSAO = 5
+JANELA_DILATACAO = 5
 
-JANELA_H = 157
-JANELA_W = 157
-
-M_H = 5
-M_W = 5
-
-LIMIAR = 0.2
+THRESHOLD = 0.2
 #===============================================================================
-
-def createBuffer(rows, cols, img_aux):
-    buffer = np.zeros(img_aux.shape)
-
-    ## Percorre na linha, acumulando os valores a esquerda
-    for linha in range(rows):
-        for coluna in range(cols):
-            ## Primeira coluna da imagem copia o valor
-            if(linha == 0 ):
-                buffer[linha][coluna] = img_aux[linha][coluna]
-
-            # Buffer = Intensidade no pixel + Pixel a esquerda
-            buffer[linha][coluna] = img_aux[linha][coluna] + buffer[linha][coluna-1]
-
-    ## Percorre por linha
-    ## Acumulando o valor que esta em cima do pixel no buffer
-    ## Completa a imagem integral
-    for linha in range(1,rows):
-        for coluna in range(cols):
-            # Buffer = Intensidade no buffer + Valor do buffer em cima
-            buffer[linha][coluna] = buffer[linha][coluna] + buffer[linha-1][coluna]
-
-    return buffer
-
-def calculaMedia(linha, coluna, buffer_, rows, cols):
-    media = 0
-
-    superior = linha - int(JANELA_H/2) - 1
-    if(superior < 0 ):
-        superior = -1
-
-    inferior = linha + int(JANELA_H/2)
-    if (inferior > rows - 1 ):
-        inferior = rows - 1
-
-    esquerda = coluna - int(JANELA_W/2) - 1
-    if(esquerda < 0 ):
-        esquerda = -1
-
-    direita = coluna + int(JANELA_W/2)
-    if (direita > cols - 1 ):
-        direita = cols - 1
-
-    # Estabelece os limites da janela
-    # Quando nas bordas, a janela e menor
-    janela_w = direita - esquerda
-    janela_h = inferior - superior
-
-    # Definindo os 4 pontos
-    soma = buffer_[inferior][direita]
-    if(superior >= 0 ):
-        soma = soma - buffer_[superior][direita]
-    if(esquerda >= 0 ):
-        soma = soma - buffer_[inferior][esquerda]
-    if(superior >= 0 and esquerda >= 0 ):
-        soma = soma + buffer_[superior][esquerda]
-
-    media = soma / (janela_w * janela_h)
-
-    return media
-
-def Integral(img):
-
-    rows, cols = img.shape
-
-    # Imagem integral
-    buffer = createBuffer(rows, cols, img)
-
-    img_out = np.zeros(img.shape)
-
-    # Janela deslizante
-    for linha in range(rows):
-        for coluna in range(cols):
-            img_out[linha][coluna] = calculaMedia(linha, coluna, buffer, rows, cols)
-
-    return img_out
 
 def Erosao(img):
     print("\tErode")
@@ -110,13 +29,16 @@ def Erosao(img):
     rows, cols = img.shape
     img_out = np.zeros(img.shape)
 
-    for linha in range(int(M_H/2),rows-int(M_H/2)):
-        for coluna in range(int(M_W/2),cols-int(M_W/2)):
+    for linha in range(int(JANELA_EROSAO/2),rows-int(JANELA_EROSAO/2)):
+        for coluna in range(int(JANELA_EROSAO/2),cols-int(JANELA_EROSAO/2)):
             and_ = True
-            for y in range(linha-int(M_H/2),linha+int(M_H/2)+1):
-                for x in range(coluna-int(M_W/2),coluna+int(M_W/2)+1):
+            for y in range(linha-int(JANELA_EROSAO/2),linha+int(JANELA_EROSAO/2)+1):
+                for x in range(coluna-int(JANELA_EROSAO/2),coluna+int(JANELA_EROSAO/2)+1):
                     if(img[y][x] == 0 ):
                         and_ = False
+                        break
+                if(not and_):
+                    break
 
             img_out[linha][coluna] = and_
 
@@ -127,30 +49,46 @@ def Dilata(img):
     rows, cols = img.shape
     img_out = np.zeros(img.shape)
 
-    for linha in range(int(M_H/2),rows-int(M_H/2)):
-        for coluna in range(int(M_W/2),cols-int(M_W/2)):
+    for linha in range(int(JANELA_DILATACAO/2),rows-int(JANELA_DILATACAO/2)):
+        for coluna in range(int(JANELA_DILATACAO/2),cols-int(JANELA_DILATACAO/2)):
             or_ = False
-            for y in range(linha-int(M_H/2),linha+int(M_H/2)+1):
-                for x in range(coluna-int(M_W/2),coluna+int(M_W/2)+1):
+            for y in range(linha-int(JANELA_DILATACAO/2),linha+int(JANELA_DILATACAO/2)+1):
+                for x in range(coluna-int(JANELA_DILATACAO/2),coluna+int(JANELA_DILATACAO/2)+1):
                     if(img[y][x] == 1 ):
                         or_ = True
+                        break
+                if(or_):
+                    break
 
             img_out[linha][coluna] = or_
 
     return img_out
+
+def defineSigma(w, h):
+    #Slide HDR, pag 45
+    return int(min(w,h)/24)
+
+
 #Binarizacao com Treshhold local
 def LimirarizacaoAdaptativa(img):
-    blur = cv2.GaussianBlur(img, (0,0), SIGMA)  #Integral(img)
+    
     rows, cols = img.shape
-    img_bin = np.zeros(img.shape)
-    for linha in range(rows):
-        for coluna in range(cols):
-            img_bin[linha][coluna] = 0
-            if(abs(img[linha][coluna] - blur[linha][coluna]) > LIMIAR):
-                img_bin[linha][coluna] = 1
+    sigma = defineSigma(cols, rows)
+    print("\tSigma: ", sigma)
+    blur = cv2.GaussianBlur(img, (0,0), sigma)  
+    
+    img_bin = np.where( img-blur > THRESHOLD, 1, 0)
 
-    img_erosao = Dilata(Erosao(img_bin))
-    img_out = np.concatenate((img_bin,img_erosao), axis=1)
+    # img_bin = np.zeros(img.shape)
+    # for linha in range(rows):
+    #     for coluna in range(cols):
+    #         img_bin[linha][coluna] = 0
+    #         if((img[linha][coluna] - blur[linha][coluna]) > THRESHOLD):
+    #             img_bin[linha][coluna] = 1
+
+    img_erosao = Erosao(img_bin)
+    img_dilata = Dilata(img_erosao)
+    img_out = np.concatenate((img_bin,img_erosao, img_dilata), axis=1)
     return img_out
 
 def contaArroz(imagem):
@@ -160,7 +98,6 @@ def contaArroz(imagem):
     return mask
 
 def main():
-    a = 1
     for img in INPUT_IMAGE:
         print("imagem:", img)
         imagem = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
@@ -170,11 +107,16 @@ def main():
 
         # Normalizando com float
         imagem = imagem.astype (np.float32) / 255
+        
+        start_time = timeit.default_timer ()
         img_out = contaArroz(imagem)
+        print ('\tTempo: %f' % (timeit.default_timer () - start_time))
+        
         vert = np.concatenate((imagem, img_out), axis=1)
         vert = cv2.resize(vert, (int(vert.shape[1]/2), int(vert.shape[0]/2)))
         cv2.imshow (img, vert)
-        a+=1
+        cv2.imwrite (img+'_out.png', vert*255)
+    
     cv2.waitKey ()
     cv2.destroyAllWindows ()
 
